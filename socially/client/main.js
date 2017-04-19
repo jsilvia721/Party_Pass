9e456b0417faa455e10ee164dec741d6e40d5963
Michael Gallant <Michael Gallant>
2017-04-19 13:49:54 -0400

    database, and partylist added

--- socially/client/main.js
+++ socially/client/main.js
@@ -1,6 +1,22 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Parties } from '../collections/parties';
 
 angular.module('socially', [
-  angularMeteor
-]);
+    angularMeteor
+  ])
+  .component('partiesList', {
+    templateUrl: 'client/partiesList.html',
+    controllerAs: 'partiesList',
+    controller($scope, $reactive) {
+      'ngInject';
+
+      $reactive(this).attach($scope);
+
+      this.helpers({
+        parties() {
+          return Parties.find({});
+        }
+      });
+    }
+  });
